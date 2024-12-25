9316e8cac3013d0046cf41084e1421a823a6ddc5
irisbur <irisburmi@gmail.com>
2024-12-25 17:28:11 +0200

    tests: add tests for edge cases.

--- Lattice Visualizations/tests/test_closest_point.py
+++ Lattice Visualizations/tests/test_closest_point.py
@@ -15,6 +15,12 @@ class TestClosestPointAlgorithms(unittest.TestCase):
         expected = np.array([0, 0, 0])
         np.testing.assert_array_equal(result, expected, "Failed to return the correct lattice point in D3")
 
+    def test_closest_point_d3_on_lattice_negative(self):
+        point = np.array([-1, 0, 0])
+        result = closest_point_Dn(point)
+        expected = np.array([0, 0, 0])
+        np.testing.assert_array_equal(result, expected, "Failed to return the correct lattice point in D3")
+
     def test_closest_point_d3_off_lattice(self):
         point = np.array([0.5, 0.3, -0.2])
         expected = np.array([0, 0, 0])
@@ -24,6 +30,12 @@ class TestClosestPointAlgorithms(unittest.TestCase):
     def test_closest_point_d3_boundary_case(self):
         point = np.array([0.500001, 0.500001, 0.5])
         expected = np.array([1, 1, 0])
+        result = closest_point_Dn(point)
+        np.testing.assert_array_equal(result, expected, "Failed at a boundary case in D3")
+
+    def test_closest_point_d3_another_boundary_case(self):
+        point = np.array([0.5, 0.5, 0.5])
+        expected = np.array([0, 0, 0])
         result = closest_point_Dn(point)
         np.testing.assert_array_equal(result, expected, "Failed at a boundary case in D3")
 
@@ -63,8 +75,8 @@ class TestClosestPointAlgorithms(unittest.TestCase):
         np.testing.assert_allclose(result, expected, atol=1e-9, rtol=1e-7, err_msg="Failed to find the correct closest point in A2")
 
     def test_scaled_closest_point_A2(self):
-        point = np.array([3, np.sqrt(3)])
-        expected = np.array([5, 0])
+        point = np.array([3, np.sqrt(3)]) + 1e-8
+        expected = np.array([2.5, 2.5 * np.sqrt(3)])
         result = 5 * closest_point_A2(point/5)
         np.testing.assert_allclose(result, expected, atol=1e-9, rtol=1e-7,
                                    err_msg="Failed to find the correct closest point in scaled A2")
