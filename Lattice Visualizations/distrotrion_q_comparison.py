f6a96a8a4607b8f0991b09f5a723931137731054
irisbur <irisburmi@gmail.com>
2025-01-09 16:10:30 +0200

    utils: fix d4 lattice definition.

--- Lattice Visualizations/distrotrion_q_comparison.py
+++ Lattice Visualizations/distrotrion_q_comparison.py
@@ -1,7 +1,7 @@
 import time
 from nested_lattice_quantizer import (NestedLatticeQuantizer as NQuantizer,
                                       HierarchicalNestedLatticeQuantizer as HQuantizer)
-from utils import get_a2, get_d3, get_e8, calculate_mse
+from utils import get_a2, get_d3, get_d4, get_e8, calculate_mse
 from closest_point import closest_point_A2, closest_point_Dn, closest_point_E8, custom_round
 import numpy as np
 import matplotlib.pyplot as plt
